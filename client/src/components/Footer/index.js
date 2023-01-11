6d7e6091b2ac92ea2b8526ab21e0f4c04e7b8860
Sean Wallace <sean.wallace.australia@gmail.com>
2023-01-11 14:36:55 +1100

    deployment app using MONGODB_URI

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Footer = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container text-center mb-5">
+        {location.pathname !== '/' && (
+          <button
+            className="btn btn-dark mb-3"
+            onClick={() => navigate(-1)}
+          >
+            &larr; Go Back
+          </button>
+        )}
+        <h4>
+          🍞 From the Kitchen 🍗 🥮 of {' '}
+          <span
+            className="emoji"
+            role="img"
+            aria-label="heart"
+            aria-hidden="false"
+          >
+            ❤️
+          </span>{' '}
+          Valmar Western Aged Care Services.
+        </h4>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
