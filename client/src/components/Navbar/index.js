9982254e8ceb1894fb11852b616a209168041db4
Trentan Jurkans <107039183+TRN10@users.noreply.github.com>
2023-01-15 15:53:39 +1100

    login page styling corrected

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -12,26 +12,26 @@ import Auth from '../../utils/auth';
 
 function NavTop() {
     //if user logged in
-        // return logged-in-view
+    // return logged-in-view
     //else
-        // display restricted-view
-        
-    if( Auth.loggedIn() ) {
+    // display restricted-view
+
+    if (Auth.loggedIn()) {
         return (
             <Navbar>
                 <Container id="navbar">
                     <Navbar.Brand href="#home"><h2 id="nav-logo"><FontAwesomeIcon icon={faSolidIcons.faUtensils} /> lunchDate</h2></Navbar.Brand>
-                    <Nav.Link href="/addclient">add client</Nav.Link>
-                    <Nav.Link href="#home">edit client</Nav.Link>
-                    <Nav.Link href="#home">client order</Nav.Link>
-                    <Nav.Link href="#home">menu editor</Nav.Link>
+                    <Nav.Link href="/addclient">add client <FontAwesomeIcon icon={faSolidIcons.faUserPlus} /></Nav.Link>
+                    <Nav.Link href="#home">edit client <FontAwesomeIcon icon={faSolidIcons.faUserPen} /></Nav.Link>
+                    <Nav.Link href="#home">client order <FontAwesomeIcon icon={faSolidIcons.faPen} /></Nav.Link>
+                    <Nav.Link href="#home">menu editor <FontAwesomeIcon icon={faSolidIcons.faPenToSquare} /></Nav.Link>
                     <NavDropdown title="view" id="navbarScrollingDropdown">
-                        <NavDropdown.Item href="#action3">All towns</NavDropdown.Item>
+                        <NavDropdown.Item href="/landing">All towns</NavDropdown.Item>
                         <NavDropdown.Divider />
                         <NavDropdown.Item href="#action4">
                             Tumut (red)
                         </NavDropdown.Item>
-    
+
                         <NavDropdown.Item href="#action5">
                             Tumut (blue)
                         </NavDropdown.Item>
@@ -46,21 +46,21 @@ function NavTop() {
                         </NavDropdown.Item>
                     </NavDropdown>
                     {/* <Button variant="secondary" type="submit"> */}
-                    <Nav.Link href="#home" onClick={Auth.logout}>logout</Nav.Link>
+                    <Nav.Link href="#home" onClick={Auth.logout}>logout <FontAwesomeIcon icon={faSolidIcons.faRightFromBracket} /></Nav.Link>
 
 
                     {/* </Button> */}
-    
+
                     {/* <Navbar.Toggle /> */}
                     <Navbar.Collapse className="justify-content-end">
-    
+
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
         );
     }
-    else{
-        return(
+    else {
+        return (
             <Navbar>
                 <Container id="navbar">
                     <Navbar.Brand href="#home"><h2 id="nav-logo"><FontAwesomeIcon icon={faSolidIcons.faUtensils} /> lunchDate</h2></Navbar.Brand>
@@ -70,24 +70,24 @@ function NavTop() {
         )
     }
 }
-        //}
-        //     <>
-        //       <Link className="btn btn-lg btn-info m-2" to="/me">
-        //         {Auth.getProfile().data.username}'s profile
-        //       </Link>
-        //       <button className="btn btn-lg btn-light m-2" onClick={logout}>
-        //         Logout
-        //       </button>
-        //     </>
-        //   ) : (
-        //     <>
-        //       <Link className="btn btn-lg btn-info m-2" to="/login">
-        //         Login
-        //       </Link>
-        //       <Link className="btn btn-lg btn-light m-2" to="/signup">
-        //         Signup
-        //       </Link>
-        //     </>
-        //   )}
+//}
+//     <>
+//       <Link className="btn btn-lg btn-info m-2" to="/me">
+//         {Auth.getProfile().data.username}'s profile
+//       </Link>
+//       <button className="btn btn-lg btn-light m-2" onClick={logout}>
+//         Logout
+//       </button>
+//     </>
+//   ) : (
+//     <>
+//       <Link className="btn btn-lg btn-info m-2" to="/login">
+//         Login
+//       </Link>
+//       <Link className="btn btn-lg btn-light m-2" to="/signup">
+//         Signup
+//       </Link>
+//     </>
+//   )}
 
 export default NavTop; 
